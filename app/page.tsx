aade38520535ef1d2aa813db758a983eea681f57
BablooKannojia <bablookmr143@gmail.com>
2026-08-21 14:56:17 +0530

    metadata add

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import MainPage from "../src/Module/MainPage";
 
 export const metadata = {
-  title: "Loksatya News",
-  description: "Latest news, videos, photos and visual stories",
+  title: "Loksatya News: Breaking News, Latest News, News Headlines Today, India & World News",
+  description: "Stay Updated with Latest News on Loksatya News: Catch Breaking News, Top News Headlines from India and Around the World. Explore Today's Updates in Sports, Cricket, Entertainment, Business, Politics, Technology, and More. Stay Informed with Daily News and Developments Across India and Globally",
 };
 
 export default function Home() {
